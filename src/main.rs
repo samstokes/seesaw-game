e532e37abb043db14252a96d406eb30ba468d0c1
Sam Stokes <me@samstokes.co.uk>
2018-10-29 11:26:33 -0500

    Piston tutorial - spinning square

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,73 @@
+extern crate glutin_window;
+extern crate graphics;
+extern crate opengl_graphics;
+extern crate piston;
+
+use glutin_window::GlutinWindow as Window;
+use opengl_graphics::{GlGraphics, OpenGL};
+use piston::event_loop;
+use piston::input;
+use piston::window::WindowSettings;
+
+pub struct App {
+    gl: GlGraphics,
+    rotation: f64,
+}
+
+impl App {
+    fn render(&mut self, args: &input::RenderArgs) {
+        use graphics::Transformed;
+
+        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+        const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+
+        let square = graphics::rectangle::square(0.0, 0.0, 50.0);
+        let rotation = self.rotation;
+        let (x, y) = ((args.width / 2) as f64, (args.height / 2) as f64);
+
+        self.gl.draw(args.viewport(), |c, gl| {
+            graphics::clear(GREEN, gl);
+
+            let transform = c
+                .transform
+                .trans(x, y)
+                .rot_rad(rotation)
+                .trans(-25.0, -25.0);
+
+            graphics::rectangle(RED, square, transform, gl);
+        });
+    }
+
+    fn update(&mut self, args: &input::UpdateArgs) {
+        self.rotation += 2.0 * args.dt;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let opengl = OpenGL::V3_2;
+
+    let mut window: Window = WindowSettings::new("seesaw", [200, 200])
+        .opengl(opengl)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    let mut app = App {
+        gl: GlGraphics::new(opengl),
+        rotation: 0.0,
+    };
+
+    let mut events = event_loop::Events::new(event_loop::EventSettings::new());
+    while let Some(e) = events.next(&mut window) {
+        use input::RenderEvent;
+        use input::UpdateEvent;
+
+        if let Some(r) = e.render_args() {
+            app.render(&r);
+        }
+
+        if let Some(u) = e.update_args() {
+            app.update(&u);
+        }
+    }
 }
